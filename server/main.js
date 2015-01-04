5987b3af60318c6653f80875d7cce7a95c546a83
Luca Bruno <lucabru@src.gnome.org>
2015-01-04 18:45:55 +0100

    move something, make the client connect to the server and also serve the static content

--- server/main.js
+++ server/main.js
@@ -1,10 +1,10 @@
-var app = require('express')();
+var path = require('path');
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io').listen(http);
 
-app.get('/', function(req, res){
-  res.sendfile('index.html');
-});
+app.use(express.static(__dirname + '/../client'))
 
 io.on('connection', function(socket){
   console.log('a user connected');
